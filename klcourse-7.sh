073d18805a1c5ec85287f50418694b0fb18efb1e
sukcbb <52514285+sukcbb@users.noreply.github.com>
2019-07-05 18:20:31 +1000

    Create klcourse-7.sh

---
+++ klcourse-7.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+ssh cloud-user@sasviya02 wget https://github.com/sukcbb/klcourse/raw/master/inventory.sas7bdat
+ssh cloud-user@sasviya02 sudo cp inventory.sas7bdat /opt/sas/viya/config/var/lib/evmsvrops/evdm/log/log/inventory.sas7bdat
+ssh cloud-user@sascas01 sudo /etc/init.d/sas-viya-cascontroller-defaults stop
+ssh cloud-user@sascas01 sudo /etc/init.d/sas-viya-cascontroller-defaults start
+ssh cloud-user@sasviya02 sudo /etc/init.d/sas-viya-ops-agentsrv-default restart
