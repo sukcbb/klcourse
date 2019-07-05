a092a51290ac4bfdf28e134ef250d139aaab423f
sukcbb <52514285+sukcbb@users.noreply.github.com>
2019-07-05 16:16:59 +1000

    Update klcourse7.sh

--- klcourse7.sh
+++ klcourse7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -l "cloud-user" "sasviya02"
-ssh -o PasswordAuthentication=no cloud-user@sasviya02
+ssh -o StrictHostKeyChecking=no -l "cloud-user" "intviya02"
+ssh -o PasswordAuthentication=no cloud-user@intviya02
 wget https://github.com/sukcbb/klcourse/raw/master/inventory.sas7bdat
 sudo cp inventory.sas7bdat /opt/sas/viya/config/var/lib/evmsvrops/evdm/log/log/inventory.sas7bdat
 sudo /etc/init.d/sas-viya-ops-agentsrv-default restart
