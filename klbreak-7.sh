648591edcb9255b28cc1c4a3f844144b1817063b
sukcbb <52514285+sukcbb@users.noreply.github.com>
2019-07-05 16:58:29 +1000

    Update klbreak-7.sh

--- klbreak-7.sh
+++ klbreak-7.sh
@@ -1,4 +1,2 @@
 #!/bin/bash -x
-ssh -o StrictHostKeyChecking=no -l "cloud-user" "sasviya02" wget https://github.com/sukcbb/klcourse/raw/master/inventory.sas7bdat
-ssh -o StrictHostKeyChecking=no -l "cloud-user" "sasviya02" sudo cp inventory.sas7bdat /opt/sas/viya/config/var/lib/evmsvrops/evdm/log/log/inventory.sas7bdat
-ssh -o StrictHostKeyChecking=no -l "cloud-user" "sasviya02" sudo /etc/init.d/sas-viya-ops-agentsrv-default restart
+wget https://github.com/sukcbb/klcourse/raw/master/inventory.sas7bdat
