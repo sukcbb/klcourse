49cb2c1a851a2c6e5d9285173a78d420c7caf933
sukcbb <52514285+sukcbb@users.noreply.github.com>
2019-07-05 17:16:23 +1000

    Create ssh-noprompt.sh

---
+++ ssh-noprompt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+ssh -o StrictHostKeyChecking=no -l "cloud-user" "sasviya02"
+ssh -o StrictHostKeyChecking=no -l "cloud-user" "sasviya03"
+ssh -o StrictHostKeyChecking=no -l "cloud-user" "sascas01"
+ssh -o StrictHostKeyChecking=no -l "cloud-user" "sascas02"
+ssh -o StrictHostKeyChecking=no -l "cloud-user" "sascas03"
