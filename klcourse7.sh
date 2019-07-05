bf48dc2abbeb56c8bb67e214f2380e2d92f1e85d
sukcbb <52514285+sukcbb@users.noreply.github.com>
2019-07-05 16:13:22 +1000

    Update klcourse7.sh

--- klcourse7.sh
+++ klcourse7.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -x
 ssh -o StrictHostKeyChecking=no -l "cloud-user" "sasviya02"
 ssh -o PasswordAuthentication=no cloud-user@sasviya02
 wget https://github.com/sukcbb/klcourse/raw/master/inventory.sas7bdat
