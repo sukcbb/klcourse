57b3fc141aaca325f4bf1db9483d925c68501c50
sukcbb <52514285+sukcbb@users.noreply.github.com>
2019-07-05 16:08:16 +1000

    Update klcourse7.sh

--- klcourse7.sh
+++ klcourse7.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -x
 
+COMPLEX="false"
+if [ "$1" = "complex" ];
+    then
+        COMPLEX="true"
+        echo You have accepted the challenge!
+fi
+
+
+if grep -q -i "release 6" /etc/redhat-release ; then
+  majversion=6
+elif grep -q -i "release 7" /etc/redhat-release ; then
+  majversion=7
+else
+  echo "Apparently, running neither release 6.x nor 7.x "
+  exit;
+fi
+
 ssh -o StrictHostKeyChecking=no -l "cloud-user" "sasviya02"
 ssh -o PasswordAuthentication=no cloud-user@sasviya02
 wget https://github.com/sukcbb/klcourse/raw/master/inventory.sas7bdat
